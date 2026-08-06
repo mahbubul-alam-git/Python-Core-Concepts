fdd97859a7c4ee876e580eb8df7acf95fccf41cf
Mahbubul Alam <sakibgo24@gmail.com>
2026-08-06 12:21:12 +0600

    Refactor warm_up.py for user input and greeting

--- 01_Basics/warm_up.py
+++ 01_Basics/warm_up.py
@@ -1,15 +1,22 @@
 # print funtion uses...
-
 print("Hello World")
 print("Twinkle, twinkle, little star,\nHow I wonder what you are!\nUp above the world so high,\nLike a diamond in the sky.")
-
 
 # Simple funtion doing sum
 def add_numbers(num1,num2):
     return (num1 + num2)
 
-f = float(input("Enter your first digit:"))
-s = float(input("Enter your second digit:"))
+user_input_01 = float(input("Enter your first digit:"))
+user_input_02 = float(input("Enter your second digit:"))
 
-a = add_numbers(f,s)
-print(f"Total number is {a}")
+total = add_numbers(user_input_01,user_input_02)
+print(f"Total number is {total}")
+
+# Greeting the user 
+users = input("Please enter your name: ")
+print(f"Good Afternoon {users}\nKindly scan your ID card!")
+
+# find() method returns the index of the first occurrence of the specified value.
+rhyme = 'saki b, a  s'
+find = rhyme.find("  ")
+print(find)
