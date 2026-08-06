110b0424f1bd20f3aff33e896bed6ff77ea6058e
Mahbubul Alam <sakibgo24@gmail.com>
2026-08-06 13:41:33 +0600

    Refactor fruit input collection and printing
    Refactored code to collect seven fruit names into a list and print them.

--- 02_Data_Structures/list_dict_checking.py
+++ 02_Data_Structures/list_dict_checking.py
@@ -2,6 +2,24 @@
 user_input = str([input("Enter seven fruits name:")])
 print(f"All seven fruits you likes:\n{user_input}")
 
-a = [1,2,3,4,5]
-b = [6,7,8,9,10]
-print(sum(a+b)+4)
+# This program will take 7 fruit names from the user and store them in a list. It will then print the list of fruits.
+fruits = []
+
+f1 = input('Enter Fruit name: ')  
+fruits.append(f1)
+f2 = input('Enter Fruit name: ')  
+fruits.append(f2)
+f3 = input('Enter Fruit name: ')  
+fruits.append(f3)
+f4 = input('Enter Fruit name: ')  
+fruits.append(f4)
+f5 = input('Enter Fruit name: ')  
+fruits.append(f5)
+f6 = input('Enter Fruit name: ')  
+fruits.append(f6)
+f7 = input('Enter Fruit name: ')  
+fruits.append(f7)
+
+print(fruits)
+
+
