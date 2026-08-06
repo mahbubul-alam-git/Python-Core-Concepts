4a837178d39a55393233a6d51a32a05930fa6d57
Mahbubul Alam <sakibgo24@gmail.com>
2026-08-06 13:02:14 +0600

    Add files via upload

---
+++ 02_Data_Structures/list_dict_checking.py
@@ -0,0 +1,3 @@
+# This program takes input from the user for seven fruit names and stores them in a list. It then prints out the list of fruits that the user likes.
+user_input = str([input("Enter seven fruits name:")])
+print(f"All seven fruits you likes:\n{user_input}")
