449a6403963e2616799841831145ebf9fea45d72
Mahbubul Alam <sakibgo24@gmail.com>
2026-08-06 11:56:54 +0600

    Implement add_numbers function for summation
    Added a function to sum two numbers and print the result.

--- 01_Basics/warm_up.py
+++ 01_Basics/warm_up.py
@@ -2,3 +2,14 @@
 
 print("Hello World")
 print("Twinkle, twinkle, little star,\nHow I wonder what you are!\nUp above the world so high,\nLike a diamond in the sky.")
+
+
+# Simple funtion doing sum
+def add_numbers(num1,num2):
+    return (num1 + num2)
+
+f = float(input("Enter your first digit:"))
+s = float(input("Enter your second digit:"))
+
+a = add_numbers(f,s)
+print(f"Total number is {a}")
