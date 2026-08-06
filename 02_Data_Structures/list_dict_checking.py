fa30d568ad1706b72d155fe3aef79b052ec0af28
Mahbubul Alam <sakibgo24@gmail.com>
2026-08-06 13:08:59 +0600

    lists add sum operation

--- 02_Data_Structures/list_dict_checking.py
+++ 02_Data_Structures/list_dict_checking.py
@@ -1,3 +1,7 @@
 # This program takes input from the user for seven fruit names and stores them in a list. It then prints out the list of fruits that the user likes.
 user_input = str([input("Enter seven fruits name:")])
 print(f"All seven fruits you likes:\n{user_input}")
+
+a = [1,2,3,4,5]
+b = [6,7,8,9,10]
+print(sum(a+b)+4)
