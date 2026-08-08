45fda72033c188ec657150f8e9c2c97ca167d621
Mahbubul Alam <sakibgo24@gmail.com>
2026-08-08 11:23:38 +0600

    Implement greatest number finder in conditional_expression.py
    Added functionality to find the greatest number among four user inputs.

--- 02_Data_Structures/conditional_expression.py
+++ 02_Data_Structures/conditional_expression.py
@@ -4,3 +4,18 @@ if (users >=18 and users <70):
     print('You are eligible to vote')
 else:
     print('You are not eligible to vote')
+
+# Write 4 numbers by users and find greatest number among them
+a1 = int(input("Enter your 1st number: "))
+a2 = int(input("Enter your 2nd number: "))
+a3 = int(input("Enter your 3rd number: "))
+a4 = int(input("Enter your 4th number: "))
+
+if (a1>a2) and (a1>a3) and (a1>a4):
+    print(f"Greatest number is: {a1}")
+elif (a2>a1) and (a2>a3) and (a2>a4):
+    print(f"Greatest number is: {a2}")
+elif (a3>a2) and (a3>a1) and (a1>a4):
+    print(f"Greatest number is: {a3}")
+elif (a4>a2) and (a4>a3) and (a4>a1):
+    print(f"Greatest number is: {a4}")
