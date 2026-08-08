a8b5e5cd40146d3299d6cc8bb32f71d8acf2e350
Mahbubul Alam <sakibgo24@gmail.com>
2026-08-08 10:08:46 +0600

    Add files via upload

---
+++ 02_Data_Structures/conditional_expression.py
@@ -0,0 +1,6 @@
+# user mute be greater than 18 or equal to 18 to vote
+users = int(input('Enter your age: '))
+if (users >=18 and users <70):
+    print('You are eligible to vote')
+else:
+    print('You are not eligible to vote')
