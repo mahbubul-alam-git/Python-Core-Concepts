cf22546e540f97cf6848ac655616897fcb03b6e4
Mahbubul Alam <sakibgo24@gmail.com>
2026-08-17 14:11:54 +0600

    Implement while loop for list printing
    Added a while loop to print elements of a list.

--- 03_Loops/while_loop.py
+++ 03_Loops/while_loop.py
@@ -3,3 +3,9 @@ i = 1
 while (i<59):
     print(i)
     i = i + 1
+
+# write a program to print the content of a list
+l = [1,2,3,4,5,6,[3,32,[5,[0,4],4],0],7,8,9]
+while (len(l) > 0):
+    print(l[0])
+    l.pop(0)
