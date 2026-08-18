c98dbd8bb43b65cd489674e6b543a788bc51f946
Mahbubul Alam <sakibgo24@gmail.com>
2026-08-18 14:47:58 +0600

    Add files via upload

---
+++ 03_Loops/for_loop.py
@@ -0,0 +1,8 @@
+# write a program to print the numbers from 1 to 5 using for loop
+l = [ 1,2,3,4,5]
+for item in l:
+    print(item)
+
+# write a program to print the numbers from 0 to 10 in steps of 2
+for i in range(0,5,2):
+    print(i)
