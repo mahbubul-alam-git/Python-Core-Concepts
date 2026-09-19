f67dcb28968835ccd2bd42f9dbb1849e3f9de5ef
Mahbubul Alam <sakibgo24@gmail.com>
2026-09-19 15:18:08 +0600

    Refactor loop practices and add new patterns

--- 03_Loops/while&for_loop_prectice.py
+++ 03_Loops/while&for_loop_prectice.py
@@ -58,3 +58,12 @@ print(f'{n} factorial result is {l}')
 n = int(input('Enter the number of rows: '))
 for i in range(1,n+1):
     print(' '*(n-i) + '*'*(2*i-1)) # Print spaces and stars for each row
+'''
+08 write a program to print following pattern.
+*
+**
+***
+'''
+n = int(input('Enter the number of rows: '))
+for i in range(1, n+1):
+    print('*'*(i))
