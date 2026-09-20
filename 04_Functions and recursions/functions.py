78b9344394ea2c0143a7b965546316ceb8628ef6
Mahbubul Alam <sakibgo24@gmail.com>
2026-09-20 15:33:02 +0600

    Add files via upload

---
+++ 04_Functions and recursions/functions.py
@@ -0,0 +1,8 @@
+def func1():
+    print('Hello world') 
+
+func1() # This code defines a function named `func1` that prints "Hello world" when called. After defining the function, it is called with `func1()`, which will execute the print statement and display "Hello world" in the output.The function can be called any number of times, anywhere in the program.
+
+def greet():
+    print('Guten Tag')
+greet()
