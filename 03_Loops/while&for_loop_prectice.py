bae2132459191d25b5c9f5cb3afb5fe226ee4706
Mahbubul Alam <sakibgo24@gmail.com>
2026-09-20 15:10:24 +0600

    eine neuen code laden auf neu pattran hoch!

--- 03_Loops/while&for_loop_prectice.py
+++ 03_Loops/while&for_loop_prectice.py
@@ -67,3 +67,16 @@ for i in range(1,n+1):
 n = int(input('Enter the number of rows: '))
 for i in range(1, n+1):
     print('*'*(i))
+
+'''
+09 write a program to print following pattern.
+***
+* *
+***
+'''
+n = 50
+for i in range(1,n+1):
+    if i == 1 or i == n:
+        print('*'*n)
+    else:
+        print('*'+(' '*(n-2))+'*')
