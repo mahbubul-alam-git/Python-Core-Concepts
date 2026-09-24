b6f28e0c5507922fd6f7d61da39ef5ad47bb99fd
Mahbubul Alam <sakibgo24@gmail.com>
2026-09-24 16:24:04 +0600

    Add Celsius to Fahrenheit conversion function
    Added a function to convert Celsius to Fahrenheit in addition to the existing greatest number function.

--- 04_Functions and recursions/precticeset.py
+++ 04_Functions and recursions/precticeset.py
@@ -11,3 +11,11 @@ def greatest (a,b,c):
     if c > b and c > a:
         print(f"The greatest numbers is {c}")
 greatest(a,b,c)
+
+# 02 write a program to convert Celsius to Fahrenheit.
+c = float(input("Enter Celsius for converting to Fahrenheit: "))
+def convert(c):
+    f = (c * 1.8) + 32  #formula= f = (cx1.8)+32 
+    print(f"{c}C to {f:.2f}F")
+
+convert(c)
